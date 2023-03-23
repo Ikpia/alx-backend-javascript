a2403222b074c1cbf1b81b1565a0fd1a3763dad6
Ikpia <echukwuka97@gmail.com>
2023-03-23 07:25:23 +0100

    commit file

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,4 +1,3 @@
-import returnHowManyArguments from './4-rest-parameter.js';
+import { taskFirst, taskNext } from './0-constants.js';
 
-console.log(returnHowManyArguments('one'));
-console.log(returnHowManyArguments('one', 'two', 3, '4th'));
+console.log(`${taskFirst()} ${taskNext()}`);
